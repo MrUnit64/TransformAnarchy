0ba4d13abb2c20db1a8c3c05a8c56b4c31f5646a
pikethom <tartturtle20@gmail.com>
2022-08-07 01:06:14 +1200

    Add world space text to tell if in Local or Global Also toggles that now also fixed the bug with keybinds lol

---
+++ RotationAnarchy/Components/RAWorldSpaceUI.cs
@@ -0,0 +1,55 @@
+﻿namespace RotationAnarchy 
+{
+
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Parkitect.UI;
+    using TMPro;
+    using RotationAnarchy.Internal;
+
+    public class RAWorldSpaceText : ModComponent
+    {
+
+        // World Space texts
+        private UIWorldSpaceText LocalSpaceText;
+
+        public static string[] worldAxesNames = new string[]
+        {
+                "Global",
+                "Local"
+        };
+
+        public override void OnApplied()
+        {
+            LocalSpaceText = GameObject.Instantiate(UIAssetManager.Instance.uiWorldSpaceTextGO, UIWorldSpaceController.Instance.transform);
+            LocalSpaceText.text.color = Color.white;
+            LocalSpaceText.text.fontSize = 48f;
+        }
+
+        public override void OnReverted()
+        { 
+            GameObject.Destroy(LocalSpaceText);
+        }
+
+        public override void OnUpdate()
+        {
+            if (RA.Controller.ActiveGhost && RA.Controller.ActiveBuilder && RA.Controller.GameState == ParkitectState.Placement)
+            {
+                var Ghost = RA.Controller.ActiveGhost;
+
+                LocalSpaceText.transform.position = Ghost.transform.position;
+                //LocalSpaceText.rectTransform.ForceUpdateRectTransforms();
+                LocalSpaceText.transform.up = Vector3.up;
+
+                LocalSpaceText.text.text = (RA.LocalRotationHotkey.Pressed) ? "Local" : "Global";
+
+                LocalSpaceText.gameObject.SetActive(true);
+
+            }
+            else
+            {
+                LocalSpaceText.gameObject.SetActive(false);
+            }
+        }
+    }
+}
